12cccedb1d85e1aa9e7b57959bc66f288eaf1684
Danny Caulwell <caulwell@protonmail.com>
2022-04-11 22:12:05 +0100

    display dictionary information upon selecting word

--- client/src/LyricSelection.js
+++ client/src/LyricSelection.js
@@ -9,6 +9,12 @@ const StyledSelectedLyric = styled.div`
     flex-direction: column;
     width: 95%;
     padding: 1rem 1rem;
+    overflow-y: auto; 
+`;
+
+const Heading = styled.div`
+    
+
 `;
 
 
@@ -18,13 +24,57 @@ const WordContainer = styled.div`
 
 const WordButton = styled.button`
 
+    ${({ first }) => first && `
+        border-top-left-radius: 7px;
+        border-bottom-left-radius: 7px;
+  `}
+  ${({ last }) => last && `
+        border-top-right-radius: 7px;
+        border-bottom-right-radius: 7px;
+  `}
+  ${({ selected }) => selected && `
+        background-color: lime;
+  `}
+
+
+    width: 4rem;
+    height: 2rem;
+`;
+
+const DefinitionContainer = styled.div`
+
+`;
+
+const DefinitionsList = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const Definition = styled.div`
+    height: 8rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const DefinitionTitle = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const DefinitionBody = styled.div`
+    display: flex;
+    flex-direction: column;
 `;
 
 
 export default function LyricSelection({selectedLine,}){
 
     const [words, setWords] = useState([]);
-    const [defintions, setDefinitions] = useState({});
+    const [selectedWord, setSelectedWord] = useState("");
+    const [definitions, setDefinitions] = useState({});
+
 
 
     useEffect(() =>{
@@ -39,13 +89,13 @@ export default function LyricSelection({selectedLine,}){
     
     const handleGetDefinition = e => {
         const word = e.currentTarget.getAttribute("name");
+        setSelectedWord(word);
         axios.get("http://localhost:3001/definition", {
             params: {
                 word
             }
         })
         .then(res => {
-            console.log(res.data);
             setDefinitions(res.data);
         });
     };
@@ -57,16 +107,53 @@ export default function LyricSelection({selectedLine,}){
         <StyledSelectedLyric>
         {selectedLine && 
         <>
+        <Heading>
             <h3>
                 {selectedLine.translation}
             </h3>
             <WordContainer>
-                {words.map(word => {
+                {words.map((word, index) => {
                     return (
-                        <WordButton name={word} onClick={e => handleGetDefinition(e)}>{word}</WordButton>
+                        <WordButton name={word} selected={word === selectedWord} last={index === words.length-1} first ={index === 0} onClick={e => handleGetDefinition(e)}>{word}</WordButton>
                     )
                 })}
             </WordContainer>
+            
+        </Heading>
+            
+            <DefinitionContainer>
+                {definitions &&
+                <>
+                    <h2>{selectedWord} {definitions.wordType && `- ${definitions.wordType}`}</h2>
+                    {definitions.gender && 
+                        <h5>Gender: {definitions.gender}</h5>
+                    }
+                    {definitions.pronounciation && 
+                        <h5>Pronounciation: {definitions.pronounciation}</h5>
+                    }
+
+                    <DefinitionsList>
+                        {definitions.definitions && definitions.definitions.map(definition => {
+                            return (
+                                <Definition>
+                                    <DefinitionTitle>
+                                    {definition.translations && <div><strong>{definition.translations}</strong>  </div>}
+                                    {definition.domain &&<div>  Domain: {definition.domain}</div>}
+                                     {definition.usage &&<div> Usage: {definition.usage}</div>}
+                                    </DefinitionTitle>
+                                    <DefinitionBody>
+                                        {definition.spanishDef && <div>Spanish Definition: {definition.spanishDef}</div>}
+                                        {definition.exampleSpan &&<div> Example: {definition.exampleSpan}</div>}
+                                        {definition.exampleTrans && <div>Example: {definition.exampleTrans}</div>}
+                                    </DefinitionBody>
+                                </Definition>
+                            )
+                        })}
+                    </DefinitionsList>
+                </>
+                   
+                }
+            </DefinitionContainer>
         </>
         }
        
